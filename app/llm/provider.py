d782f5fab8652b62f04c1ad889db99a6d00d506c
nefro313 <robinkphilip.com>
2025-05-15 16:38:17 +0530

    Provider setup

---
+++ app/llm/provider.py
@@ -0,0 +1,24 @@
+
+from typing import Dict, List, Optional, Any
+from app.core.config import settings
+import logging
+from crewai import LLM
+logger = logging.getLogger(__name__)
+
+
+class GroqProvider:
+    pass
+
+class GoogleProvider:
+    """Google AI provider implementation"""
+    def gemini_llm(self):
+        llm = LLM(
+        model="gemini/gemini-2.0-flash",
+        api_key= settings.GOOGLE_API_KEY
+        )
+        return llm
+
+
+class OpenAIProvider:
+    """OpenAI provider implementation"""
+    pass
